454db05b5c66af35cd580a244c97499b16fc496c
Emilly Danciguer <danciguere@gmail.com>
2025-03-04 18:57:58 -0400

    Criação da Classe Autor e Conexao com o Banco de Dados

--- GestaoBiblioteca/Form1.cs
+++ GestaoBiblioteca/Form1.cs
@@ -8,19 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using GestaoBiblioteca.Formulario;
+using MySql.Data.MySqlClient;
 
 namespace GestaoBiblioteca
 {
     public partial class Form1 : Form
     {
+        string strconexao = "server=localhost;port=3306;uid=root;pwd=danciguer16;database=trabalho_interdisciplinar";
+
         public Form1()
         {
             InitializeComponent();
         }
 
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            // Conexão com o Banco de Dados - Gestão Biblioteca
 
+            MySqlConnection conexao = new MySqlConnection(strconexao);
+            try
+            {
+                conexao.Open();
+                MessageBox.Show("Conexão com o banco de dados estabelecida com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar com o Banco de Dados: " + ex.Message);
+            }
         }
 
         private void btCadastrar_Click(object sender, EventArgs e)
---
+++ GestaoBiblioteca/Models/Autor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace GestaoBiblioteca.Models
+{
+    public class Autor
+    { 
+        public int _ID_autor { get; set; }
+        public string _nome { get; set; }
+        public DateTime _data_nasc { get; set; }
+        public string _nacionalidade { get; set; }
+        public string _cpf { get; set; }
+        public string _email { get; set; }
+        public string _telefone { get; set; }
+    }
+}
